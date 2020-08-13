67a498d8f9cf3a6d421a29d998da71f0650f239e
milena-m-m <milenamoreira166@gmail.com>
2020-08-13 20:15:50 -0300

    segunda versao

--- exemplo_excecao/excecao.java
+++ exemplo_excecao/excecao.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package exemplo_excecao;
 
-/**
- *
- * @author Milena
- */
 public class excecao {
-    
+    public static void main(String[] args) {
+        System.out.println("Consegui");
+        
+        System.out.println("Segundo commit");
+    }
 }
